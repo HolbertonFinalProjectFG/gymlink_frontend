f5d217676fc0764e96a328e50a89de00cf6ced69
Facusan2016 <facusan2016@gmail.com>
2024-02-25 10:24:46 -0300

    added loader to mgGroup and added as an UI element

--- src/components/Trainer/MgGroup/MgGroup.jsx
+++ src/components/Trainer/MgGroup/MgGroup.jsx
@@ -1,29 +1,29 @@
-import { AddNew } from '../AddNew';
-import { Mg } from './Mg';
-import { useState, useEffect } from 'react';
+import { AddNew } from '../AddNew'
+import { Mg } from './Mg'
+import { useState, useEffect } from 'react'
 import { DragOverlay, useDroppable } from '@dnd-kit/core'
-import { gymlink } from "../../../api/gymlink"
-import { AddMg } from "../Form/AddMg"
+import { gymlink } from '../../../api/gymlink'
+import { AddMg } from '../Form/AddMg'
+import { Loader } from '../../Ui/Loader/Loader'
 
 export const MgGroup = ({ active }) => {
-
   const [show, setShow] = useState(false)
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState(null)
+  const [loading, setLoading] = useState(false)
   const { attributes, setNodeRef } = useDroppable({
     id: 'mg_groups'
   })
 
   useEffect(() => {
     setLoading(true)
-    gymlink.get("/api/mg").then(({data}) => {
-        setData(data.data);
-      })
+    gymlink.get('/api/mg').then(({ data }) => {
+      setData(data.data)
+    })
       .catch((err) => {
         console.log(err)
       })
       .finally(() => setLoading(false))
-  }, []);
+  }, [])
 
   const showFormAddMg = () => {
     setShow(true)
@@ -33,27 +33,24 @@ export const MgGroup = ({ active }) => {
     <section
       ref={setNodeRef}
       {...attributes}
-      className="overflow-y-auto overflow-x-hidden flex flex-1 items-center justify-start flex-row h-[95%] flex-wrap gap-5 p-5 border-4 border-light-secondary rounded-xl">
+      className="overflow-y-auto overflow-x-hidden flex flex-col flex-1 items-center justify-left h-[95%] gap-5 p-5 border-4 border-light-secondary rounded-xl max-w-fit">
       <AddNew fnc={() => showFormAddMg()} long={true}/>
       {
         loading && (
-          <h2>Loading...</h2>
+          <Loader/>
         )
-      }
-      {
-        !loading && !data && (<h2>Something failed.</h2>)
       }
       {
         !loading && data && data.map((e, idx) => <Mg key={idx} {...e}/>)
       }
       <DragOverlay>
         {
-          active?.id ?
-          <Mg {...active.data.current}/>
-          : null
+          active?.id
+            ? <Mg {...active.data.current}/>
+            : null
         }
       </DragOverlay>
       <AddMg show={show}/>
     </section>
-  );
-};
+  )
+}
---
+++ src/components/Ui/Loader/Loader.jsx
@@ -0,0 +1,12 @@
+export const Loader = () => {
+  return (
+    <div className="
+    flex relative items-center
+    justify-center p-2 w-20 h-20
+    border-8 border-light-primary_light
+    rounded-full border-b-light-primary
+     animate-spin
+    ">
+    </div>
+  )
+}
