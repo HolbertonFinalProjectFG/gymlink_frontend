7bee783833720fe5a583fb60e7d1e8cbf7fea487
Facusan2016 <facusan2016@gmail.com>
2024-02-26 19:48:07 -0300

    fix global css styles to root and repsonsiveness of login page

--- src/components/Auth/LoginForm.jsx
+++ src/components/Auth/LoginForm.jsx
@@ -2,7 +2,6 @@ import { Link } from "react-router-dom"
 import { useForm } from "../../hooks/useForm"
 import { useNavigate } from "react-router-dom"
 import axios from 'axios'
-import Cookies from "js-cookie"
 import { useState } from "react"
 
 const initialState = {
@@ -41,25 +40,25 @@ export const LoginForm = () => {
   }
 
   return (
-    <section className="w-full h-full flex flex-[2] flex-col items-center justify-center relative z-20 bg-light-secondary_op bg-opacity-70">
+    <section className="w-full h-full flex flex-[2] flex-col items-center justify-center relative z-20 p-8">
       <span
-        className={` bg-red-400 px-4 py-1
+        className={`bg-red-400 px-4 py-1
         rounded-xl text-light-secondary
         transition-opacity
-        ${loginError ? 'opacity-100' : 'opacity-0'}`}
+        ${loginError ? 'opacity-100 h-fit' : 'opacity-0'}`}
         >Not valid credentials
       </span>
-      <form className="flex flex-col w-full max-w-[25rem] justify-start bg-light-backg p-10 m-4 rounded-lg">
-        <h1 className="text-4xl font-bold pb-1.5">Login</h1>
+      <form className="flex flex-col w-full max-w-[25rem] md:w-[22rem] sm:w-full sm:max-w-[20rem] justify-start bg-light-backg p-10 sm:p-8 m-4 rounded-lg">
+        <h1 className="text-4xl sm:text-3xl font-bold pb-1.5">Login</h1>
         <p className="text-sm text-login-fontLight text-gray-500">Sign in into your account</p>
-        <div className="flex flex-col pt-8 gap-6">
-          <input name="email" value={email} onChange={onInputChange} className="h-12 p-2.5 rounded-lg bg-transparent border border-gray-500 border-opacity-50 focus:outline-none focus:border-gray-700 transition ease-in-out" type="text" placeholder="Email"></input>
-          <input name="password" value={password} onChange={onInputChange} className="h-12 p-2.5 rounded-lg bg-transparent border border-gray-500 border-opacity-50 focus:outline-none focus:border-gray-700 transition ease-in-out"  type="password" placeholder="Password"></input>
+        <div className="flex flex-col pt-8 sm:pt-4 gap-6 sm:gap-4">
+          <input name="email" value={email} onChange={onInputChange}       className="p-2.5 sm:text-sm rounded-lg sm:rounded-md bg-transparent border border-gray-500 border-opacity-50 focus:outline-none focus:border-gray-700 transition ease-in-out" type="text" placeholder="Email"></input>
+          <input name="password" value={password} onChange={onInputChange} className="p-2.5 sm:text-sm rounded-lg sm:rounded-md bg-transparent border border-gray-500 border-opacity-50 focus:outline-none focus:border-gray-700 transition ease-in-out"  type="password" placeholder="Password"></input>
         </div>
         <Link to={'/'} className="text-sm text-login-accent pt-4 pl-1 hover:text-light-primary transition-colors ease-in-out">Forgot password?</Link>
-        <button onClick={handleSubmit} type="submit" className="p-2 mt-20 font-bold bg-login-accent text-login-form rounded-lg text-xl bg-light-primary text-white hover:scale-[0.99] transition ease-in-out">Login</button>
+        <button onClick={handleSubmit} type="submit" className="p-2 mt-20 sm:mt-10 font-bold bg-login-accent text-login-form rounded-lg text-xl sm:text-base bg-light-primary text-white hover:scale-[0.99] transition ease-in-out">Login</button>
         <hr className="h-px my-4 border-0 bg-gray-400"/>
-        <p className="text-gray-400 text-center mt-4">Need an account? <Link className="underline text-login-accent text-light-primary transition-colors ease-in-out" to={'/'}>Talk with us</Link></p>
+        <p className="text-gray-400 text-center mt-4 sm:text-sm">Need an account? <Link className="underline text-login-accent text-light-primary transition-colors ease-in-out" to={'/'}>Talk with us</Link></p>
       </form>
     </section>
   )
